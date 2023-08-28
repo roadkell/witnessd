e255213eb6aeb784b4a0cb5e0594bd73875fa490
Kelly Roadkill <4406611+roadkell@users.noreply.github.com>
2023-08-28 08:19:19 +0500

    feat: :lipstick: add description to sample script

--- examples/dir-size-limiter.sh
+++ examples/dir-size-limiter.sh
@@ -3,9 +3,18 @@
 # Limit directory to a certain size.
 # When quota is exceeded, delete oldest file(s) recursively.
 
-echo "dir-size-limiter: when directory size is above quota, delete oldest file(s)"
+thedir=$1
+quota=$2
+dirsize=$(du -sk "$thedir" | cut -f1)
 
-echo "$1"
-echo "$2"
+echo
+echo "dir-size-limiter: when directory size is above quota, delete oldest file(s)"
+echo
+echo "WARNING! This script has no safety/sanity checks. Use at your own risk!"
+echo "(Currently it does nothing though)"
+echo
+echo "Directory to limit: $thedir"
+echo "Current directory size=$dirsize"
+echo "Quota: $quota"
 
 echo "dir-size-limiter: done!"
